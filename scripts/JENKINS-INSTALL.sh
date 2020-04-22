ec0da705cc333cf1121ad3b3b3717d9a17377297
Saravanan Arumugam <41325803+aswath2saru@users.noreply.github.com>
2020-04-23 02:05:35 +0530

    Update JENKINS-INSTALL.sh

--- scripts/JENKINS-INSTALL.sh
+++ scripts/JENKINS-INSTALL.sh
@@ -3,8 +3,9 @@
 
 sudo apt-get update
 sudo apt-get -y install openjdk-8-jdk gnupg rng-tools docker.io
-wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
-sudo apt-get -y install jenkins git
-sudo service jenkins start
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
