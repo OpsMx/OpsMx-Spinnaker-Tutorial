2f0434be30305410f5e09ffd32c6aaae9ee26f46
Saravanan Arumugam <41325803+aswath2saru@users.noreply.github.com>
2020-04-23 15:33:25 +0530

    Update JENKINS-INSTALL.sh

--- scripts/JENKINS-INSTALL.sh
+++ scripts/JENKINS-INSTALL.sh
@@ -3,10 +3,11 @@
 
 
 sudo apt-get update
-sudo apt-get -y install openjdk-8-jdk gnupg rng-tools docker.io
-wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt update
-sudo apt install jenkins
+# Install Jenkins
+## Before install is necessary to add Jenkins to trusted keys and source list
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins
 sudo systemctl start jenkins
 sudo systemctl status jenkins
